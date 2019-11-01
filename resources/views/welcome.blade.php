eb06e7242f7bbe24759aab112d7177388d57313a
mohamed <mohamed.elhassak2@gmail.com>
2019-11-01 23:54:40 +0100

    pre-heroku

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Backend wirtingApp
+                    Backend wirtingApp mobile app
                 </div>
 
                 <div class="links">
